7eab004665f5b77ade0e75c4c907a6ab319103d4
imanamini <iman.fa88@gmail.com>
2021-08-21 02:09:07 +0430

    ref #18

--- plugins/bootstrap.js
+++ plugins/bootstrap.js
@@ -5,7 +5,6 @@ if (process.client) {
   window.jQuery = jQuery
   window.$ = jQuery
   require('bootstrap')
-
   // require('@/node_modules/video.js/dist/video.js')
   require('owl.carousel')
   require('jquery-ui')
